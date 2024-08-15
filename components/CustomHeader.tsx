a2473b97ecc7a4d75482ab01750a2539c8d2589c
lmaluke <lerekomaduke@gmail.com>
2024-08-15 12:21:53 +0200

    Custom Header: Design Searchbar

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,7 +1,25 @@
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import React from 'react'
 import Colors from '@/constants/Colors'
+import { Link } from 'expo-router';
+
+const SearchBar = () => (
+    <View style={styles.searchContainer}>
+        <View style={styles.searchSection}>
+            <View style={styles.searchField}>
+                <Ionicons style={styles.searchIcon} name="ios-search" size={20} color={Colors.medium} />
+                <TextInput style={styles.input} placeholder="Restaurants, groceries, dishes" />
+            </View>
+
+            <Link href={'/'} asChild>
+                <TouchableOpacity style={styles.optionButton}>
+                <Ionicons name="options-outline" size={20} color={Colors.primary} />
+                </TouchableOpacity>
+            </Link>
+        </View>
+    </View>
+)
 
 const CustomHeader = () => {
   return (
@@ -23,6 +41,8 @@ const CustomHeader = () => {
             <Ionicons name='person-outline' size={20} color={Colors.primary}></Ionicons>
         </TouchableOpacity>
       </View>
+
+      <SearchBar></SearchBar>
     </SafeAreaView>
   )
 }
@@ -36,6 +56,41 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         gap: 20,
+    },
+
+    searchContainer: {
+        height: 60,
+        backgroundColor: 'white'
+    },
+
+    searchSection: {
+        flexDirection: 'row',
+        gap: 10,
+        flex: 1,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+    },
+      
+    searchField: {
+        flex: 1,
+        backgroundColor: Colors.lightGrey,
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+      
+    input: {
+        padding: 10,
+        color: Colors.mediumDark,
+    },
+      
+    searchIcon: {
+        paddingLeft: 10,
+    },
+
+    optionButton: {
+        padding: 10,
+        borderRadius: 50,
     },
 
     bike: {
