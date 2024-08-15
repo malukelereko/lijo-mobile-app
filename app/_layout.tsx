eccc195ebe4a2dd9ae384d848e4edc2036f7db4b
lmaluke <lerekomaduke@gmail.com>
2024-08-15 21:38:50 +0200

    Filter Modal: Design Filter modal page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, useNavigation } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { TouchableOpacity, useColorScheme } from 'react-native';
 import CustomHeader from '@/components/CustomHeader';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 
 
@@ -16,7 +18,7 @@ export const unstable_settings = {
 
 
 export default function RootLayoutNav() {
-
+  const navigation = useNavigation();
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -26,7 +28,30 @@ export default function RootLayoutNav() {
             
             header: () => <CustomHeader />
            }} />
+
+        <Stack.Screen 
+          name="(modal)/filter" 
+          options={{
+            presentation: 'modal',
+            headerTitle: 'Filter',
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: Colors.lightGrey
+            },
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.goBack();
+                }}>
+                <Ionicons name="close-outline" size={28} color={Colors.primary} />
+              </TouchableOpacity>
+            ),
+          }}
+           
+        />
       </Stack>
+
+      
     </BottomSheetModalProvider>
   );
 }
